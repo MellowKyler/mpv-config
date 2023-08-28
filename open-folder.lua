f7b9223f49bb1aab4bfcb2ee97e9912d6c62c5ff
MellowKyler <108599378+MellowKyler@users.noreply.github.com>
2023-08-28 08:06:10 -0500

    Add files via upload

---
+++ open-folder.lua
@@ -0,0 +1,15 @@
+-- redundancy
+    --local dir, filename = utils.split_path(path)
+    --local dir = mp.get_property("working-directory")
+    --local video_file = mp.get_property("working-directory") .. "/" .. mp.get_property("filename")
+    --mp.command_native({ name = "subprocess", capture_stdout = true, playback_only = false, args = args })
+
+utils = require 'mp.utils'
+
+function open_folder()
+    local path = mp.get_property('path')
+    args = { 'nemo', path }
+    utils.subprocess({ args = args })
+end
+
+mp.add_key_binding("Ctrl+e", "open-folder", open_folder)
