46632eac7f39a001c15d38fd2abe129f718bc4b3
MellowKyler <108599378+MellowKyler@users.noreply.github.com>
2024-01-13 08:43:39 -0600

    Create chapters_to_skip.lua

---
+++ scripts/.utils/chapters_to_skip.lua
@@ -0,0 +1,13 @@
+
+
+local patterns = {
+    "OP","[Oo]pening$", "^[Oo]pening:", "[Oo]pening [Cc]redits",
+    "ED","[Ee]nding$", "^[Ee]nding:", "[Ee]nding [Cc]redits", "Closing",
+    "[Pp]review$", "Next Prev.", "Next", "Preview", "PV",
+    --Temporary
+    "Recap","End Credits", "Fin",
+    --"Chapter 01", "Chapter 04", "Chapter 05", "Chapter 06"
+    --"Chapter 1", "Chapter 4", "Chapter 5",
+    --"Prologue",
+}
+return patterns
